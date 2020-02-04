c626c09ef43a5b14ae7a34de119a1307d18a55b0
Ryan French <ryan@ryanfrench.co>
2020-02-04 10:44:19 +0000

    Add version command

---
+++ cmd/version.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Returns the version of aws-role",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("aws-role - version: " + rootCmd.Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
