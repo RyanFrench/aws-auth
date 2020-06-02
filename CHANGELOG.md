99dfd190550e3f0e3bd696bea96472edf1bc1867
Ryan French <ryan@ryanfrench.co>
2020-06-02 12:43:53 +0100

    Automate upload of packages to release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## 0.3.0
+
+### Added
+- Deploy package to github via [ghr](https://github.com/tcnksm/ghr)
+
+### Changed
+- Return the version without extra text in command line
+
 ## 0.2.0
 
 ### Added
--- cmd/version.go
+++ cmd/version.go
@@ -10,7 +10,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Returns the version of aws-role",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("aws-role - version: " + rootCmd.Version)
+		fmt.Println(rootCmd.Version)
 	},
 }
 
